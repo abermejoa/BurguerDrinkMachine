ecd1d609a10e48e244a58b3cf201ec2fd5871091
jorge murillo <jmurilloc@minsait.com>
2025-01-20 14:03:28 +0100

    Implement, map of drinks in UtilFile Finished getDrinks(String name)

---
+++ src/main/java/app/controller/IUtilFile.java
@@ -0,0 +1,16 @@
+package app.controller;
+
+import app.model.drinks.Drink;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+public interface IUtilFile {
+
+    Map<Integer, Drink> getDrinks(String name) throws IOException;
+
+    void addDrink(List<Drink> drinks);
+    int countDrinks();
+
+}
