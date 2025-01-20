2fd3f25e574b01c6387dd0eef00f72dd710bd357
jorge murillo <jmurilloc@minsait.com>
2025-01-20 14:42:36 +0100

    Finish beta - UtilFile

--- src/main/java/app/controller/IUtilFile.java
+++ src/main/java/app/controller/IUtilFile.java
@@ -2,6 +2,7 @@ package app.controller;
 
 import app.model.drinks.Drink;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
@@ -10,7 +11,8 @@ public interface IUtilFile {
 
     Map<Integer, Drink> getDrinks(String name) throws IOException;
 
-    void addDrink(List<Drink> drinks);
-    int countDrinks();
+    void addDrink(List<Drink> drinks) throws IOException;
+    int countDrinks() throws IOException;
+    List<Drink> recolectSalesDrinksData() throws IOException;
 
 }
--- src/main/java/app/controller/UtilFile.java
+++ src/main/java/app/controller/UtilFile.java
@@ -2,23 +2,29 @@ package app.controller;
 
 import app.model.drinks.Drink;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.*;
 
+/**
+ *
+ */
 public class UtilFile implements IUtilFile{
-
-    File fichero;
-    //AHORA HAGO EL SINGLETON
 
     private static UtilFile instance;
 
+    /**
+     *
+     */
     private UtilFile(){
         System.out.println("Recogiendo datos");
     }
 
+    /**
+     *
+     * @return
+     */
     public static UtilFile getInstance(){
         if (instance==null){
             instance = new UtilFile();
@@ -27,7 +33,13 @@ public class UtilFile implements IUtilFile{
     }
 
 
-
+    /**
+     *
+     * @param name
+     * @return Map<Integer,Drink>
+     * @throws IOException
+     * @throws NumberFormatException
+     */
     @Override
     public Map<Integer, Drink> getDrinks(String name) throws IOException,NumberFormatException {
         Map<Integer,Drink> drinks = new HashMap<>();
@@ -37,25 +49,73 @@ public class UtilFile implements IUtilFile{
             //CAMPO 0 -> NOMBRE
             //CAMPO 1 -> PRECIO
             //CAMPO 2 -> DESCRIPCIÓN
-            drinks.put(contador++,new Drink(campos[0],campos[1],precioToFloat(campos[2])));
+            drinks.put(contador++,new Drink(campos[0],campos[2],precioToFloat(campos[1])));
         }
         return drinks;
     }
 
+    /**
+     *
+     * @param drinks
+     * @throws IOException
+     */
     @Override
-    public void addDrink(List<Drink> drinks) {
+    public void addDrink(List<Drink> drinks) throws IOException {
+        try(
+                FileOutputStream fileOutputStream = new FileOutputStream(rutaBuilder("SalesDrinks"));
+                ObjectOutputStream oos = new ObjectOutputStream(fileOutputStream)
+        ) {
+                oos.writeObject(drinks);
+                System.out.println("Se han guardado los objetos");
 
+        }
     }
 
+    /**
+     *
+     * @return
+     * @throws IOException
+     */
     @Override
-    public int countDrinks() {
-        return 0;
+    public int countDrinks() throws IOException {
+        return recolectSalesDrinksData().size();
     }
 
+    /**
+     *
+     * @return
+     * @throws IOException
+     * @throws RuntimeException
+     */
+    @Override
+    public List<Drink> recolectSalesDrinksData() throws IOException, RuntimeException {
+        List<Drink> drinks;
+        try(
+                FileInputStream fileInputStream = new FileInputStream(rutaBuilder("SalesDrinks"));
+                ObjectInputStream ois = new ObjectInputStream(fileInputStream)
+        ) {
+            drinks = (List<Drink>) ois.readObject();
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        return drinks;
+    }
+
+    /**
+     *
+     * @param name
+     * @return
+     */
     private String rutaBuilder(String name){
         return "database/"+name+".txt";
     }
 
+    /**
+     *
+     * @param precio
+     * @return
+     * @throws NumberFormatException
+     */
     private float precioToFloat(String precio)throws NumberFormatException{
         return Float.parseFloat(precio);
     }
@@ -63,6 +123,10 @@ public class UtilFile implements IUtilFile{
 
 class Demo{
     public static void main(String[] args) throws IOException {
-        Map<Integer, Drink> drinksMap = UtilFile.getInstance().getDrinks("ColdDrinks");
+        Map<Integer, Drink> drinksMap = UtilFile.getInstance().getDrinks("TeeDrinks");
+        System.out.println(drinksMap);
+        List<Drink> bebidas = drinksMap.values().stream().toList();
+        UtilFile.getInstance().addDrink(bebidas);
+        System.out.println(UtilFile.getInstance().recolectSalesDrinksData());
     }
 }
