507f50a5f8354f801dabb5b4bbbf52ddb97f24a5
abermejoa <abermejoa@minsait.com>
2025-01-20 13:55:31 +0100

    Drink class added

--- src/main/java/app/model/drinks/Drink.java
+++ src/main/java/app/model/drinks/Drink.java
@@ -1,4 +1,41 @@
 package app.model.drinks;
 
-public class Drink {
+public class Drink implements IDrink{
+    private String name, description;
+    private float price;
+
+    public Drink(String name, String description, float price){
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    @Override
+    public String prepare() {
+        return String.format("This %s is delicious", name);
+    }
 }
---
+++ src/main/java/app/model/drinks/IDrink.java
@@ -0,0 +1,6 @@
+package app.model.drinks;
+
+public interface IDrink {
+
+    String prepare();
+}
