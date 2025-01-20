e910dc623b25a32bde1d515f7c15fc1322b689fe
iggura <igutierrezr@minsait.com>
2025-01-20 17:55:37 +0100

    Implementation of MachineController and DrinkController, start of Menu and App logic

---
+++ src/main/java/app/controller/DrinkController.java
@@ -0,0 +1,12 @@
+package app.controller;
+
+import app.model.drinks.Drink;
+
+import java.io.IOException;
+import java.util.Map;
+
+public abstract class DrinkController {
+    public static Map<Integer, Drink> getDrinks(String machineName) throws IOException {
+        return UtilFile.getInstance().getDrinks(machineName);
+    }
+}
--- src/main/java/app/view/Menu.java
+++ src/main/java/app/view/Menu.java
@@ -1,7 +1,37 @@
 package app.view;
 
+import app.controller.DrinkController;
+import app.controller.MachineController;
+import org.reflections.Reflections;
+
+import java.io.IOException;
+import java.util.Set;
+
 public class Menu {
 
+    /*
+    Set<Class<? extends IHotDrinkFactory>> types =
+            new Reflections("factories") // ""
+                    .getSubTypesOf(IHotDrinkFactory.class);
+        for (Class<? extends IHotDrinkFactory> type : types)
+    {
+        namedFactories.add(new Pair<>(
+                type.getSimpleName().replace("Factory", ""),
+                type.getDeclaredConstructor().newInstance()
+        ));
+    }*/
+    public static void getMachine() {
+        MachineController.getMachines()
+                .forEach((number, machine) -> System.out.println(number + ": " + machine));
+    }
 
+    public static void getDrinks(String machineName) throws IOException {
+        DrinkController.getDrinks(machineName);
+    }
+}
 
+class Demo {
+    public static void main(String[] args) throws IOException {
+        Menu.getDrinks("CoffeController");
+    }
 }
